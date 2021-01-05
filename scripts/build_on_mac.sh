5d0072206b7bbb77a695d2e8a65690935350c568
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-05 18:12:38 +0900

    mild fix gitignore

--- scripts/build_on_mac.sh
+++ scripts/build_on_mac.sh
@@ -1,6 +1,6 @@
-git clone --depth 1 https://github.com/abulmo/edax-reversi.git edax
+git clone --depth 1 https://github.com/abulmo/edax-reversi.git
 
-cd edax
+cd edax-reversi
 mkdir -p data
 curl -OL https://github.com/abulmo/edax-reversi/releases/download/v4.4/eval.7z
 7z x eval.7z -y
@@ -13,8 +13,8 @@ chmod +x bin/mEdax
 
 cd ../
 cp edax.ini build/edax.ini
-cp -r edax/bin build/bin
-cp -r edax/data build/data
+cp -r edax-reversi/bin build/bin
+cp -r edax-reversi/data build/data
 
 mkdir -p build
 dart2native bin/edax_runner.dart -v -o build/edax_runner-mac
