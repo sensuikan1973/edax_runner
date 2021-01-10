62eff12a0b01ed09ebc31cc4533dccf13bab87e1
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-11 02:06:16 +0900

    remove unnecessary async

--- test/learner_test.dart
+++ test/learner_test.dart
@@ -3,7 +3,7 @@ import 'package:edax_runner/edax_runner_command.dart';
 import 'package:edax_runner/learner.dart';
 import 'package:test/test.dart';
 
-Future<void> main() async {
+void main() {
   const bookFile = 'dummy';
   const learningListFile = 'test/learning_list_test.txt';
   const learnedLogFile = 'test/learned_log.txt';
@@ -15,7 +15,7 @@ Future<void> main() async {
   const firstLine = '// comment_1';
   const secondLine = '// comment_2';
   const thirdLine = 'F5F6E6F4G5E7E3F3C5C4G3C6D6D7C3C2B5D3B4E2F2H3F1D1D2G4G6C1E1B6C7D8F7H4F8G7';
-  await srcFile.writeAsString('$firstLine\n$secondLine\n$thirdLine');
+  srcFile.writeAsStringSync('$firstLine\n$secondLine\n$thirdLine');
 
   tearDownAll(() {
     srcFile.deleteSync();
