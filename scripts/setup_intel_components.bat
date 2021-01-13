a08d2ec1573ab7bd757e5c36871a78d775360825
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-14 03:48:11 +0900

    icl windows

--- scripts/setup_intel_components.bat
+++ scripts/setup_intel_components.bat
@@ -1,2 +1,3 @@
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
 @call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
+icl -v
