ff8c3918eff4922c1f227c30555ca11240c30f6f
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-16 07:26:46 +0900

    fix

--- scripts/setup_intel_components.bat
+++
@@ -1,2 +0,0 @@
-for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
-@call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
