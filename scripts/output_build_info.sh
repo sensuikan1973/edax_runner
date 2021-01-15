7ed2a777a0d344736e2fd92f4b8f40aa565c1f13
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-16 08:54:28 +0900

    fix

--- scripts/output_build_info.sh
+++ scripts/output_build_info.sh
@@ -8,19 +8,17 @@ chmod 777 $dst_file
 
 echo "=== os image ===" >> $dst_file
 echo $ImageOS >> $dst_file
-echo -n -e "\n"
 
 echo "=== github sha ===" >> $dst_file
 echo $GITHUB_SHA >> $dst_file
-echo -n -e "\n"
 
 echo "=== dart version ===" >> $dst_file
 dart --version >> $dst_file 2>&1
-echo -n -e "\n"
-
-echo "=== $2 version ===" >> $dst_file
-$2 --version >> $dst_file
 
 echo "=== edax-reversi sha ===" >> $dst_file
 cd edax-reversi
 git rev-parse HEAD >> ../$dst_file
+
+cd ..
+echo "=== $2 version ===" >> $dst_file
+$2 --version >> $dst_file
