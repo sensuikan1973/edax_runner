ab2fde2e75c18d585277f4befa86cc3220a93804
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-16 08:41:39 +0900

    fix

--- scripts/output_build_info.sh
+++ scripts/output_build_info.sh
@@ -4,18 +4,19 @@
 dst_file="$1/env.txt"
 
 touch $dst_file
+chmod 777 $dst_file
 
 echo "=== os image ===" >> $dst_file
 echo $ImageOS >> $dst_file
-echo ""
+echo -n -e "\n"
 
 echo "=== github sha ===" >> $dst_file
 echo $GITHUB_SHA >> $dst_file
-echo ""
+echo -n -e "\n"
 
 echo "=== dart version ===" >> $dst_file
 dart --version >> $dst_file 2>&1
-echo ""
+echo -n -e "\n"
 
 echo "=== $2 version ===" >> $dst_file
 $2 --version >> $dst_file
