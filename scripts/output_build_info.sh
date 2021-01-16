25752c17180497df6e901f9ce95b613b1de960ab
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-16 09:38:31 +0900

    mild fix

--- scripts/output_build_info.sh
+++ scripts/output_build_info.sh
@@ -5,19 +5,19 @@ dst_file="$1/env.txt"
 
 touch $dst_file
 
-echo "=== os image ===" >> $dst_file
-echo $ImageOS >> $dst_file
-
 echo "=== edax-runner sha ===" >> $dst_file
 echo $GITHUB_SHA >> $dst_file
-
-echo "=== dart version ===" >> $dst_file
-dart --version >> $dst_file 2>&1
 
 echo "=== edax-reversi sha ===" >> $dst_file
 cd edax-reversi
 git rev-parse HEAD >> ../$dst_file
-
 cd ..
+
+echo "=== os image ===" >> $dst_file
+echo $ImageOS >> $dst_file
+
+echo "=== dart version ===" >> $dst_file
+dart --version >> $dst_file 2>&1
+
 echo "=== $2 version ===" >> $dst_file
 $2 --version >> $dst_file
