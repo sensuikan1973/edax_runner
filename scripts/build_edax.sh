e47d0c7625ef206785ebef1933db089764deab91
sensuikan1973 <sensuikan1973@gmail.com>
2022-05-07 08:08:49 +0900

    use sensuikan1973 libedax_sensuikan1973 branch

--- scripts/build_edax.sh
+++ scripts/build_edax.sh
@@ -8,10 +8,11 @@ set -euxo pipefail
 # edax_bin_name (e.g. mEdax)
 # dst: (e.g. build)
 
-git clone --depth 1 https://github.com/abulmo/edax-reversi.git
-
-cd edax-reversi || exit
-git checkout c783ef998aca86a7ae9358a58645222df27ae5a0
+rm -rf edax-reversi
+git clone https://github.com/sensuikan1973/edax-reversi
+cd edax-reversi
+git remote update --prune
+git switch libedax_sensuikan1973
 
 mkdir -p data
 curl -OL https://github.com/abulmo/edax-reversi/releases/download/v4.4/eval.7z
@@ -20,7 +21,6 @@ curl -OL https://github.com/abulmo/edax-reversi/releases/download/v4.4/eval.7z
 mkdir -p bin
 cd src || exit
 $edax_build_command
-# doxygen # big document. too heavy.
 
 cd ../bin || exit
 chmod +x "$edax_bin_name"
@@ -33,4 +33,3 @@ cp resources/edax.ini "$dst/edax.ini"
 cp resources/learning_list.txt "$dst/learning_list.txt"
 cp -r edax-reversi/bin "$dst/bin"
 cp -r edax-reversi/data "$dst/data"
-# cp -r edax-reversi/doc/html "$dst/doc/html"
